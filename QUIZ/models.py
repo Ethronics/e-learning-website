01cf4d988c7162fadb5929b4fcbb18870130c7d7
bina-na <137456354+bina-na@users.noreply.github.com>
2024-08-13 01:57:15 +0300

    Update models.py for quiz

--- QUIZ/models.py
+++ QUIZ/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from CURRICULUM.models import Lesson
+from USER.models import Instructor
 
 class Quiz_Question(models.Model):
     CHOICE_A = 'A'
@@ -13,12 +14,12 @@ class Quiz_Question(models.Model):
         (CHOICE_C , 'C'),
         (CHOICE_D , 'D')
         ) 
-    question = models.CharField(max_length=300, unique=True, verbose_name='Question')
-    option1 = models.CharField(max_length=300 , verbose_name= 'A')
-    option2 = models.CharField(max_length=300 , verbose_name= 'B')
-    option3 = models.CharField(max_length=300, verbose_name= 'C')
-    option4 = models.CharField(max_length=300, verbose_name= 'D')
-    answer = models.CharField(max_length=300, choices=CHOICES, verbose_name='Answer')
+    question = models.CharField(max_length=255, unique=True, verbose_name='Question')
+    option1 = models.CharField(max_length=255 , verbose_name= 'A')
+    option2 = models.CharField(max_length=255 , verbose_name= 'B')
+    option3 = models.CharField(max_length=255, verbose_name= 'C')
+    option4 = models.CharField(max_length=255, verbose_name= 'D')
+    answer = models.CharField(max_length=255, choices=CHOICES, verbose_name='Answer')
     correct_answer = models.CharField(max_length=255)
 
     def __str__(self):
@@ -26,10 +27,19 @@ class Quiz_Question(models.Model):
 
 
 class Quiz(models.Model):
+    instructor = models.ForeignKey(Instructor , on_delete=models.PROTECT , related_name='quiz_creator')
     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
     Question = models.ForeignKey(Quiz_Question, on_delete=models.CASCADE)
     date = models.DateTimeField()
 
     def __str__(self):
         return f"Quiz for {self.lesson.title}"
+    
+class QuizResult(models.Model):
+    Quiz = models.ForeignKey(Quiz , on_delete=models.PROTECT)
+    no_of_question = models.PositiveIntegerField(max_length=255)
+    right_answer = models.PositiveSmallIntegerField(max_length=255)
+    wrong_answer = models.PositiveSmallIntegerField(max_length=255)
+    TotalResult = models.PositiveSmallIntegerField(max_length=255)
+
 
