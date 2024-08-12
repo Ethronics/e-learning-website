3d610435addf3f18a8b1c12a81487b1ccb3648f9
bina-na <137456354+bina-na@users.noreply.github.com>
2024-08-13 01:54:38 +0300

    Update models.py

--- ASSIGNMENT/models.py
+++ ASSIGNMENT/models.py
@@ -4,25 +4,26 @@ from USER.models import Student,Instructor
 
 
 class Assignment(models.Model):
-   title = models.CharField(max_length=255, help_text="assignmet number, eg, Assignment1")
+   number = models.IntegerField()
+   title = models.CharField(max_length=255)
    description = models.TextField()
-   document = models.FileField(upload_to='assignments/', blank=True, null=True)  # Document for the assignment
+   document = models.FileField(upload_to='assignments/', blank=True, null=True)
    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='assignments')
    instructor = models.ForeignKey(Instructor, on_delete=models.CASCADE, related_name='assignments_creator')
-   deadline = models.DateTimeField()
+   addmision_date = models.DateTimeField(auto_now_add=True)
+   Submission_date = models.DateTimeField(auto_now_add=True)
+   grade = models.IntegerField(default=0, blank=True, null=True)
+
 
    def __str__(self):
        return f"{self.title} : {self.course}"
 
-   class Meta:
-       ordering = ['-deadline']
-
+   
 
 class StudentAssignmentSubmit(models.Model):
    assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE, related_name='student_assignments')
    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='assignments_submitted')
    document = models.FileField(upload_to='student_assignments/', blank=True, null=True)
-   grade = models.IntegerField(default=0, blank=True, null=True)
    submitted_at = models.DateTimeField(auto_now_add=True)
 
    def __str__(self):
