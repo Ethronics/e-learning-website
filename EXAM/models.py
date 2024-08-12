52e9d602029ee214eb6ca2df93347fef677bfa88
bina-na <137456354+bina-na@users.noreply.github.com>
2024-08-13 01:56:04 +0300

    Update models.py

--- EXAM/models.py
+++ EXAM/models.py
@@ -13,12 +13,12 @@ class Question(models.Model):
     )
     TEST_EXAM = 'T'
     MID_EXAM = 'M'
-    FIAL_EXAM = 'F'
+    FINAL_EXAM = 'F'
     
     EXAM_TYPES = (
         (TEST_EXAM, 'Test Exam'),
         (MID_EXAM, 'Mid Exam'),
-        (FIAL_EXAM, 'Final Exam'),
+        (FINAL_EXAM, 'Final Exam'),
     )
 
     CHOICE_A = 'A'
@@ -32,12 +32,12 @@ class Question(models.Model):
         (CHOICE_C , 'C'),
         (CHOICE_D , 'D')
         ) 
-    question = models.CharField(max_length=300, unique=True, verbose_name='Question')
-    option1 = models.CharField(max_length=300 , verbose_name= 'A')
-    option2 = models.CharField(max_length=300 , verbose_name= 'B')
-    option3 = models.CharField(max_length=300, verbose_name= 'C')
-    option4 = models.CharField(max_length=300, verbose_name= 'D')
-    answer = models.CharField(max_length=300, choices=CHOICES, verbose_name='Answer')
+    question = models.CharField(max_length=255, unique=True, verbose_name='Question')
+    option1 = models.CharField(max_length=255 , verbose_name= 'A')
+    option2 = models.CharField(max_length=255 , verbose_name= 'B')
+    option3 = models.CharField(max_length=255, verbose_name= 'C')
+    option4 = models.CharField(max_length=255, verbose_name= 'D')
+    answer = models.CharField(max_length=255, choices=CHOICES, verbose_name='Answer')
     difficulty = models.CharField(max_length=1, choices=DIFFICULTY_LEVELS )
     question_for = models.CharField(max_length=10, choices=EXAM_TYPES )
     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
@@ -45,7 +45,8 @@ class Question(models.Model):
     def __str__(self):
         return f"{self.question} - {self.difficulty}"
 
-
+class ExamDuration(models.Model):
+    duration = models.PositiveIntegerField(max_length=255)
 class Exam(models.Model):
     TEST_EXAM = 'T'
     MID_EXAM = 'M'
@@ -56,6 +57,7 @@ class Exam(models.Model):
         (MID_EXAM, 'Mid Exam'),
         (FIAL_EXAM, 'Final Exam'),
     )
+    duration = models.ForeignKey(ExamDuration, on_delete=models.PROTECT)
     exam_type = models.CharField(max_length=1 , choices=EXAM_TYPES , default=TEST_EXAM)
     course = models.ForeignKey(Course , on_delete=models.PROTECT , related_name="exam_belongs_to")
     Question = models.ForeignKey('Question', on_delete=models.CASCADE)
@@ -68,8 +70,8 @@ class Exam(models.Model):
 
 class ExamResult(models.Model):
     exam = models.ForeignKey('Exam' , on_delete=models.PROTECT)
-    no_of_questions = models.PositiveIntegerField()
-    right_answers = models.PositiveIntegerField()
-    wrong_answers = models.PositiveIntegerField()
-    total_result = models.PositiveIntegerField()
+    no_of_questions = models.PositiveIntegerField(max_length=255)
+    right_answers = models.PositiveSmallIntegerField(max_length=255)
+    wrong_answers = models.PositiveSmallIntegerField(max_length=255)
+    total_result = models.PositiveSmallIntegerField(max_length=255)
     result_per_cent = models.DecimalField(max_digits=2 , decimal_places=2)
