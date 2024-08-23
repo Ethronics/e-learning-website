f8d083afba086aca5db8c0ea9f33c7f246d9b548
HayatHassen <163301578+HayatHassen@users.noreply.github.com>
2024-08-23 05:53:10 -0700

    logout page

---
+++ frontend/src/pages/auth/Logout.js
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+
+const Logout = () => {
+  const { logout } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate.push('/login');
+  };
+
+  return (
+    <button onClick={handleLogout} className="btn btn-red">Logout</button>
+  );
+};
+
+export default Logout;
